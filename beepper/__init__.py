2d53bd9c03ea6a206794fb7adca65b87d0513118
Henrik Koski <henrikkoski00@gmail.com>
2025-10-06 20:09:36 +0300

    Release 1.1.2

--- beepper/__init__.py
+++ beepper/__init__.py
@@ -1,9 +1,1 @@
-from typing import TYPE_CHECKING
-from . import beepper as _beepper
-
-beep = _beepper.beep
-
-if TYPE_CHECKING:
-    from .beepper import beep as beep
-
-__all__ = ["beep"]
+from .beepper import beep as beep
